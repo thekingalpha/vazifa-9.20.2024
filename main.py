534bb66d1a1a20278118a6e54796b8c33f79bdec
thekingalpha <usultanov079@gmail.com>
2024-10-11 13:58:19 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+# def tugilgan_yil_hisobla():
+#     ism = input("Iltimos, ismingizni kiriting: ")
+#     yosh = int(input("Iltimos, yoshingizni kiriting: "))
+#     joriy_yil = 2024  # Joriy yilni oldindan belgilaymiz
+#     tugilgan_yil = joriy_yil - yosh
+#     return f"{ism}, sizning tug'ilgan yilingiz {tugilgan_yil}-yil."
+#
+# tugilgan_yil_hisobla()
+
+# 2 misol
+
+#
+# def son_kvadrat_kub(son):
+#     kvadrat = son ** 2
+#     kub = son ** 3
+#     return f"Son: {son}\nKvadrat: {kvadrat}\nKub: {kub}"
+
+
+# 3 misol
+
+#
+# def juft_yoki_toq(son):
+#     if son % 2 == 0:
+#         return f"{son} - juft son."
+#     else:
+#         return f"{son} - toq son."
+
+
+# 4 misol
+
+
+# def kattasini_top(son1, son2):
+#     if son1 > son2:
+#         return f"{son1} - katta son."
+#     elif son2 > son1:
+#         return f"{son2} - katta son."
+#     else:
+#         return "Sonlar teng."
